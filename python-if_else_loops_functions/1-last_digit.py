bbe44d3049c05dd1d5640722ef93bba1e766870a
MarcP70 <marcpourias@yahoo.fr>
2023-05-30 05:22:56 -0700

    Fix: the sign of the modulo if the number is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
+
 if number < 0:
-    last_digit = last_digit * -1
+    last_digit = ((number * -1) % 10) * -1
+else:
+    last_digit = number % 10
+
 if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit < 6:
