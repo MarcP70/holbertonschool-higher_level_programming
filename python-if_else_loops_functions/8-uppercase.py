52d773c44200536c435311e1b57d81bb68da9bd5
MarcP70 <marcpourias@yahoo.fr>
2023-05-31 05:21:31 -0700

    Fix: delet condition to check if str is NULL on file 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if (str):
-        for char in str:
-            if (ord(char) in range(97, 123)):
-                char = chr(ord(char)-32)
-            print("{}".format(char), end="")
-        print()
+    for char in str:
+        if (ord(char) in range(97, 123)):
+            char = chr(ord(char)-32)
+        print("{}".format(char), end="")
+    print()
