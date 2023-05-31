b910819fa69aa7484ee77b9fb588f9b92976d289
MarcP70 <marcpourias@yahoo.fr>
2023-05-31 05:18:02 -0700

    Fix: delet on print in file 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     if (str):
         for char in str:
             if (ord(char) in range(97, 123)):
-                print("{}".format(chr(ord(char)-32)), end="")
-            else:
-                print("{}".format(char), end="")
+                char = chr(ord(char)-32)
+            print("{}".format(char), end="")
         print()
