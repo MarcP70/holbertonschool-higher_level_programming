ec9f4bb88bcad829ecf6b182faa3ebc067245395
MarcP70 <marcpourias@yahoo.fr>
2023-06-02 04:39:50 -0700

    Fix: delete the condition argc>0 in file 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     argc = len(argv)
 
-    if argc > 0:
-        result = sum(int(arg) for arg in argv)
-        print("{}".format(result))
+    result = sum(int(arg) for arg in argv)
+    print("{}".format(result))
