9791cfc3035ea8ef447a7913dcb369ea9c8e02e3
MarcP70 <marcpourias@yahoo.fr>
2023-06-05 14:51:24 +0200

    Fix: return None if the matrix is empty in file 6-print_matrix_integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
-        for row in matrix:
-            for num in row:
-                print("{:d}".format(num), end=" ")
-    print()
+    if matrix is None:
+        return
+    
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
