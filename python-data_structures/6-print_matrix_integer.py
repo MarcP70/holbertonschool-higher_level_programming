0b5be59510f4de8bb7841a60239430de09f476cd
MarcP70 <marcpourias@yahoo.fr>
2023-06-05 15:06:24 +0200

    Fix: the space in output in file 6-print_matrix_integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return
-    
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
-        print()
+    if matrix is not None:
+        for row in matrix:
+            i = 1
+            for num in row:
+                if i != len(row):
+                    print("{:d}".format(num), end=" ")
+                else:
+                    print("{:d}".format(num), end="")
+                i += 1
+            print()
