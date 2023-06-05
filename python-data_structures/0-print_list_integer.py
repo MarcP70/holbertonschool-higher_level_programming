a9e94b4b1d29366ebb4bb5434680a4ff29287216
MarcP70 <marcpourias@yahoo.fr>
2023-06-05 11:27:52 +0200

    Fix: the print string in file 0-print_list_integer

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for arg in my_list:
-        print("{}".format(my_list[arg-1]))
+    for num in my_list:
+        print("{}".format(num))
