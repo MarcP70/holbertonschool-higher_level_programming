0050582350fd5a02066f3a2dbf0745efec94f624
MarcP70 <marcpourias@yahoo.fr>
2023-06-07 10:01:49 +0200

    Fix: return None if no score found on file 10-best_score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     
-    best_key = 0
+    best_key = None
     best_score = 0
 
     for key, value in a_dictionary.items():
