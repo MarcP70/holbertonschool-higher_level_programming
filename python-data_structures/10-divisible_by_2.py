3e5adfc35bc7139a3a83ec590b14d56dad54204a
MarcP70 <marcpourias@yahoo.fr>
2023-06-08 13:20:47 +0200

    Fix: missing whitespace around operator on file 10-divisible_by_2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list =[]
+    new_list = []
     for element in my_list:
-        if element %2 == 0:
+        if element % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
