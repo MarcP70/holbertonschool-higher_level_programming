ddb53b787c44c3dac614dd63f5fb7697b942247c
MarcP70 <marcpourias@yahoo.fr>
2023-06-13 15:44:45 +0200

    Fix: condition float + <0 in file 4-print_square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,6 +10,8 @@ def print_square(size):
     size - the size length of the square.
     """
 
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
