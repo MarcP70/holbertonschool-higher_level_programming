d530d36911e21f6280754304b1265d49d47dae96
MarcP70 <marcpourias@yahoo.fr>
2023-06-13 12:02:34 +0200

    Fix: docstring on file 0-add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Defines an integer addition function."""
+"""
+This module implement the methode add_integer.
+
+
+"""
 
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
-
-    Float arguments are typecasted to ints before addition is performed.
-
-    Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
     """
+    This methode return the sum of two integers.
+    """
+
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
