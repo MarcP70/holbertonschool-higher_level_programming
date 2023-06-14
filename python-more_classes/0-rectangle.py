7996de369509319be1dd58191e3e4917093d7ad8
MarcP70 <marcpourias@yahoo.fr>
2023-06-14 13:15:17 +0200

    Add: file 0-rectangle to write an empty class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module includes a class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+    """
+
+    def __init__(self):
+        """
+        This methode initializes a new instance of the rectangle class.
+        Parameters:
+        - self: The Rectangle instance being initialized.
+        """
+        pass    # An empty block
