996b01c25146f978b1e75b7cb115b3b826fe30b1
MarcP70 <marcpourias@yahoo.fr>
2023-06-14 12:19:11 +0200

    Add: file 6-max_integer_test.py to test the file 6-max_integer.py

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,30 +8,34 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_empty_list(self):
-        result = max_integer([])
-        self.assertIsNone(result)
+        self.assertEqual(max_integer([]), None)
 
-    def test_single_element_list(self):
-        result = max_integer([42])
-        self.assertEqual(result, 42)
+    def test_single_element(self):
+        self.assertEqual(max_integer([1]), 1)
 
-    def test_positive_numbers(self):
-        result = max_integer([10, 5, 8, 3, 15, 12])
-        self.assertEqual(result, 15)
+    def test_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_list_unordered(self):
+        self.assertEqual(max_integer([1, 4, 3, 2]), 4)
 
     def test_negative_numbers(self):
-        result = max_integer([-5, -10, -3, -1, -8])
-        self.assertEqual(result, -1)
+        self.assertEqual(max_integer([-1, -4, -3, -2]), -1)
 
     def test_mixed_numbers(self):
-        result = max_integer([-5, 10, -3, 0, 8])
-        self.assertEqual(result, 10)
+        self.assertEqual(max_integer([1, -4, -3, -2]), 1)
 
-    def test_duplicate_numbers(self):
-        result = max_integer([5, 5, 5, 5])
-        self.assertEqual(result, 5)
+    def test_duplicated_numbers(self):
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
 
-    def test_non_integer_elements(self):
+    def test_float_list(self):
+        self.assertEqual(max_integer([1.2, 4.1, 3.6, 2.2]), 4.1)
+
+    def test_isnone(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+            
+    def test_non_numeric_elements(self):
         with self.assertRaises(TypeError):
             max_integer([1, 2, 'a', 4, 5])
 
