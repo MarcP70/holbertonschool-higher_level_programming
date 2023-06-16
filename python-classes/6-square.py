26fe3cb68a0e8203253160cea03a716111f096c8
MarcP70 <marcpourias@yahoo.fr>
2023-06-16 12:22:09 +0200

    Fix: add new condition if value is not a tuple on line 67 in file 6-square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -66,6 +66,6 @@ class Square:
     def position(self, value):
         if value[0] < 0 or value[1] < 0 or type(value) is not tuple\
                 or len(value) != 2 or type(value[0]) is not int\
-                or type(value[1]):
+                or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
