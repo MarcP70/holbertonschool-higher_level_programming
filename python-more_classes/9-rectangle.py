9994224195bc6b18fd62d615cb6d7a91f1d318e3
MarcP70 <marcpourias@yahoo.fr>
2023-06-16 11:21:15 +0200

    Add: file 9-rectangle to returns a new Rectangle instance with width == height == size

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        
+
         self.__height = height
         self.__width = width
         Rectangle.number_of_instances += 1
