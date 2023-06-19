242814ebd94bfc67aca8917db0239e1f389c2900
MarcP70 <marcpourias@yahoo.fr>
2023-06-19 16:47:43 +0200

    Fix: test file for file 1-my_list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -11,16 +11,6 @@ class MyList(list):
         - list: The list class.
     """
 
-    def __init__(self):
-        """
-        This methode initializes a new instance of the MyList class.
-        Parameters:
-        - self: The list instance being initialized.
-        """
-
-        super().__init__()
-        pass
-
     def print_sorted(self):
         """
         Prints the list in ascending order.
