7ce741654ad6d14812f585aa03487575cc556633
MarcP70 <marcpourias@yahoo.fr>
2023-06-20 09:29:50 +0200

    Add: file 6-base_geometry to print a message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains one class.
+"""
+
+
+class BaseGeometry:
+    """
+    This class define base of geometry.
+    """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
