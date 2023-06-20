04c34a8f8251a43aec042c40619b7df428e7d89d
MarcP70 <marcpourias@yahoo.fr>
2023-06-20 10:24:49 +0200

    Add file 7-base_geometry to manupulate base of geometry

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module has a class.
+
+Raises:
+    Exception: area() is not implemented.
+    TypeError: <name> must be an integer.
+    ValueError: <name> must be greater than 0.
+"""
+
+
+class BaseGeometry:
+    """This class two methodes.
+
+    Raises:
+        Exception: area() is not implemented.
+        TypeError: <name> must be an integer.
+        ValueError: <name> must be greater than 0.
+    """
+
+    pass
+
+    def area(self):
+        """Public instance method.
+
+        Raises:
+            Exception: area() is not implemented.
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method.
+
+        Args:
+            name (string): The name.
+            value (int): The value.
+
+        Raises:
+            TypeError: <name> must be an integer.
+            ValueError: <name> must be greater than 0.
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
