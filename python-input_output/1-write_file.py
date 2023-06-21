77d99a2c7c99d1ff3f4aa22ed6474ed70167365e
MarcP70 <marcpourias@yahoo.fr>
2023-06-21 12:11:19 +0200

    Fix: doctring methode on file 1-write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,15 @@
 
 
 def write_file(filename="", text=""):
+    """This methode write into a file.
+
+    Args:
+        filename (str): The name of the file.
+        text (str): The text to write.
+
+    Returns:
+        int: The number of char written.
+    """
     with open(filename, 'w', encoding="utf-8") as f:
         nb_char_written = f.write(text)
         return nb_char_written
