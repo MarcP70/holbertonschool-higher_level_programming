91c1a70d3a48a0928b48cb370d10cba63d6e440f
MarcP70 <marcpourias@yahoo.fr>
2023-06-21 16:29:42 +0200

    Fix: documentation on file 7-add_item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This script adds command line arguments to a list stored in a JSON file.
+"""
+
+
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
