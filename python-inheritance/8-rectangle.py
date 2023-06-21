febaf9c34b8135b053c3ec1fcc5df66a94288125
MarcP70 <marcpourias@yahoo.fr>
2023-06-21 09:48:22 +0200

    Fix: import previous code on file 8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,35 +7,7 @@ This module provides classes for basic geometric shapes such as rectangles
 """
 
 
-class BaseGeometry:
-    """
-    Base class for geometric shapes.
-    """
-
-    def area(self):
-        """
-        Calculate the area of the geometric shape.
-        This method raises an Exception since it is not implemented in
-            the base class.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate that the value is an integer greater than 0.
-
-        Args:
-            name (str): The name of the value.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
