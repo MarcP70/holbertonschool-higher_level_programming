5dad81a14faccbc938c6009ce393de70c19bb6a1
MarcP70 <marcpourias@yahoo.fr>
2023-06-21 10:57:26 +0200

    Fix: change the filename on file 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,6 +9,5 @@ def read_file(filename=""):
     Args:
         filename (str): The name of the file.
     """
-    with open('my_file_0.txt', encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
