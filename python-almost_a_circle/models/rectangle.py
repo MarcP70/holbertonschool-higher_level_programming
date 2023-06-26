d68f5d96dc719f7be25f85b522f16d711a60614c
MarcP70 <marcpourias@yahoo.fr>
2023-06-26 11:08:11 +0200

    Add: On file rectanglepy, adding validation of all setter methods and instantiation (id excluded)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """
 Rectangle module - Contains the Rectangle class that inherits from Base.
 """
+
 from models.base import Base
 
 
@@ -9,6 +10,7 @@ class Rectangle(Base):
     """
     A class representing a rectangle.
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Initialize a Rectangle object.
@@ -22,10 +24,10 @@ class Rectangle(Base):
 
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -48,8 +50,13 @@ class Rectangle(Base):
 
         Raises:
             ValueError: If the width value is not positive.
+            TypeError: If the width value is not an integer.
 
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -73,8 +80,13 @@ class Rectangle(Base):
 
         Raises:
             ValueError: If the height value is not positive.
+            TypeError: If the height value is not an integer.
 
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -98,8 +110,13 @@ class Rectangle(Base):
 
         Raises:
             ValueError: If the x-coordinate value is negative.
+            TypeError: If the x-coordinate value is not an integer.
 
         """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -123,6 +140,11 @@ class Rectangle(Base):
 
         Raises:
             ValueError: If the y-coordinate value is negative.
+            TypeError: If the y-coordinate value is not an integer.
 
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
