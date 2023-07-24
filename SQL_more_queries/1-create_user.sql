8047c736d5eeb0b650ce2e19ecbfcc4e04a35596
MarcP70 <marcpourias@yahoo.fr>
2023-07-24 16:18:49 +0200

    Update: file 1-create_users.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,12 +1,5 @@
 -- This script that creates the MySQL server user user_0d_1.
 -- Task 1.
--- 1-create_user.sql
-
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to the user on all databases
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Flush privileges to apply changes
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
