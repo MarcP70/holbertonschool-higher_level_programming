86aa72d18d79dbc6f05a317bcb8505134b8f11b6
MarcP70 <marcpourias@yahoo.fr>
2023-07-24 11:58:33 +0200

    Add: file 13-change_class.sql that remove all records with a 'score <= 5'

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- This script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Task 13.
+DELETE FROM second_table WHERE score <= 5;
