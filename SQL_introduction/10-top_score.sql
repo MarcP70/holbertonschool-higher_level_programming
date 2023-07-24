c5bd0939290e4915fd72fcc93e5145b280e46fb8
MarcP70 <marcpourias@yahoo.fr>
2023-07-24 11:45:58 +0200

    Add: file 10-top_score.sql that list bay best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- This script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Task 10.
+SELECT score, name FROM second_table ORDER BY score DESC;
