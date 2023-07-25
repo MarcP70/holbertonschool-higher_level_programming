fc2ee5372630a9e060a2c40a7e1a3320e5a69b56
MarcP70 <marcpourias@yahoo.fr>
2023-07-25 09:03:10 +0200

    Add: file 9-cities_by_state_join.sql that list all cities with states

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- This script that lists all cities contained in the database hbtn_0d_usa.
+-- Task 9.
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
