d70ac225cbed454eecaec8df33b8732bf07ac291
MarcP70 <marcpourias@yahoo.fr>
2023-07-31 14:23:45 +0200

    Fix: file 2-my_filter_states.py that change the format of query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -34,9 +34,9 @@ def filter_states_by_name(mysql_username, mysql_password, database_name,
     cursor = db.cursor()
 
     # Prepare the SQL query with user input using the format method
-    # to prevent SQL injection
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name_searched,))
+    cursor.execute(
+        f"SELECT * FROM states WHERE name = '{state_name_searched}'\
+              ORDER BY id ASC")
 
     # Fetch all the rows returned by the query
     results = cursor.fetchall()
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     # The script expects four command-line arguments: MySQL username,
     # password, database name, and state name to search
     mysql_username, mysql_password, database_name, state_name_searched =\
-          sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+        sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
     # Call the filter_states_by_name function with the provided arguments
     filter_states_by_name(mysql_username, mysql_password,
