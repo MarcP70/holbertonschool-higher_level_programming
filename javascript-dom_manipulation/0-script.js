7d470040cab1d8f7b91df969fc4b9fca9522878f
MarcP70 <marcpourias@yahoo.fr>
2023-09-11 12:42:12 +0200

    add file 0-script.js that updates the text color of the header element to red

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,9 @@
+// 0-script.js
+// This script updates the text color of the header element to red (#FF0000)
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the header element using document.querySelector
+  var headerElement = document.querySelector('header');
+
+  // Update the text color to red (#FF0000)
+  headerElement.style.color = '#FF0000';
+});
