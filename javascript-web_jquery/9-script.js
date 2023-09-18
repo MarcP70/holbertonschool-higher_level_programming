ca520c7a3b0ab86c3c1001511eba351ff95b3479
MarcP70 <marcpourias@yahoo.fr>
2023-09-18 11:44:43 +0200

    add file 9-script.js that fetches data from a URL and display the value translation

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,19 @@
+// Task9: this script fetches from https://hellosalut.stefanbohacek.dev/?lang=fr
+// and displays the value of hello from that fetch in the HTML tag DIV#hello
+$(document).ready(function() {
+    // URL to fetch the translation of "hello"
+    const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+
+    // Make an AJAX request to fetch the translation
+    $.ajax({
+        url: apiUrl,
+        type: 'GET',
+        success: function(data) {
+            // Update the content of the <div> with the translation
+            $('#hello').text(data.hello);
+        },
+        error: function(error) {
+            console.log('Error fetching translation:', error);
+        }
+    });
+});
