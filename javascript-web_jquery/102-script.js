12ac603db251cb451e5d4a40aef5860e7f6027a3
MarcP70 <marcpourias@yahoo.fr>
2023-09-18 12:25:56 +0200

    add file 102-script.js that  fetches and prints how to say “Hello” depending on the language

---
+++ javascript-web_jquery/102-script.js
@@ -0,0 +1,22 @@
+// Task12: This script  fetches and prints how to say “Hello” depending on the language
+$(document).ready(function() {
+  $('#btn_translate').click(function() {
+    const languageCode = $('#language_code').val();
+
+    // Check if the language code is provided
+    if (languageCode) {
+      // Make an AJAX request to fetch the translation
+      $.ajax({
+        url: `https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`,
+        type: 'GET',
+        success: function(data) {
+          // Update the content of the <div> with the translation
+          $('#hello').text(data.hello);
+        },
+        error: function(error) {
+          console.log('Error fetching translation:', error);
+        }
+      });
+    }
+  });
+});
