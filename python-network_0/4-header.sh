117c439170291a6cab0148bb10b5e65d125b53fd
MarcP70 <marcpourias@yahoo.fr>
2023-12-06 13:48:10 +0100

    fix 4-header.sh to rename the header variable

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to the URL, and displays the body of the response
-curl -s -X GET -H "School-User-Id: 98" -L $1
+curl -s -X GET -H "X-School-User-Id: 98" -L $1
