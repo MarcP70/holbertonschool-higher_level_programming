85c90da1fccfc18dfb2e91342b4e4408197cdf56
MarcP70 <marcpourias@yahoo.fr>
2023-12-06 13:30:48 +0100

    fix 3-methods.sh to change awk to cut option

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script send an OPTIONS request and display the allowed methods
-echo $(curl -sI -X OPTIONS $1 | grep "Allow" | awk '{print $2}' | tr -d '\r\n')
+echo $(curl -sI -X OPTIONS $1 | grep "Allow" | cut -d ' ' -f 2- | tr -d '\r\n')
