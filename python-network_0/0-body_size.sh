ff49648f90d16be564c60df4b077fad98124fc13
MarcP70 <marcpourias@yahoo.fr>
2023-12-06 12:04:15 +0100

    fix 0-body_size.sh to modify the code to make it 3 lines long

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # The script displays the size of the body of the URL request response
-
-# Take URL as argument
-url=$1
-
-# Use curl to send a request and display the size of the body in bytes
-size=$(curl -sI $url | grep Content-Length | awk '{print $2}' | tr -d '\r\n')
-
-# Display the size
-echo $size
+echo $(curl -sI $1 | grep Content-Length | awk '{print $2}' | tr -d '\r\n')
