521c3b2a1164b725e4672f4a556697da495385c4
MarcP70 <marcpourias@yahoo.fr>
2023-12-06 17:32:13 +0100

    add 4-hbtn_status.py to fetch https://intranet.hbtn.io/status using the requests package

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module to fetch https://intranet.hbtn.io/status using the requests package
+"""
+
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
