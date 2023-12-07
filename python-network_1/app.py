b50061827be8e405c5db2ab6b4d442f95be40ba9
MarcP70 <marcpourias@yahoo.fr>
2023-12-07 10:00:26 +0100

    fix app.py to add route for test task 8

--- python-network_1/app.py
+++ python-network_1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def status():
     return 'OK'
 
-
+# task 1
 @app.route('/get_header')
 def get_header():
     x_request_id = request.headers.get(
@@ -16,28 +16,39 @@ def get_header():
     return f'The X-Request-Id header value is: {x_request_id}'
 
 
-# task 2
+# task 2 & 6
 @app.route('/post_email', methods=['POST'])
 def post_email():
     email = request.form.get('email')
     return f'Your email is: {email}'
 
-# task 3
+# task 3 & 7
 @app.route('/')
 def index():
     return 'Index'
 
 
-# task 3
+# task 3 & 7
 @app.route('/status_401')
 def status_401():
     abort(401)
 
 
-# task 3
+# task 3 & 7
 @app.route('/status_500')
 def status_500():
     abort(500)
+
+# task 8
+@app.route('/search_user', methods=['POST'])
+def search_user():
+    letter = request.form.get('q', '')
+    if letter == 'a':
+        return jsonify({'id': 8446, 'name': 'amnirqhtfjq'})
+    elif letter == 'b':
+        return jsonify({'id': 7094, 'name': 'bmofakakhke'})
+    else:
+        return jsonify({})
 
 
 if __name__ == '__main__':
